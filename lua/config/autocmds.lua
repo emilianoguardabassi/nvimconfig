63a9857e05afd182cb65bd72c7bcb933cb922fb4
Emiliano Guardabassi <emilianojguardabassi@hotmail.com>
2024-10-26 00:07:36 -0300

    add transparency

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,26 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- Configurar fondo transparente
+vim.cmd [[
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NormalFloat guibg=NONE ctermbg=NONE
+  hi SignColumn guibg=NONE ctermbg=NONE
+  hi LineNr guibg=NONE ctermbg=NONE
+  hi EndOfBuffer guibg=NONE ctermbg=NONE
+]]
+
+
+-- Hacer el fondo transparente globalmente
+vim.api.nvim_create_autocmd({"VimEnter", "BufWinEnter", "WinEnter"}, {
+  pattern = "*",
+  callback = function()
+    vim.cmd [[
+      hi Normal guibg=NONE ctermbg=NONE
+      hi NormalFloat guibg=NONE ctermbg=NONE
+      hi SignColumn guibg=NONE ctermbg=NONE
+      hi LineNr guibg=NONE ctermbg=NONE
+      hi EndOfBuffer guibg=NONE ctermbg=NONE
+    ]]
+  end
+})
