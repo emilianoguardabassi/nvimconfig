5843918067a9bef7085201b078951f6c41bbb800
Emiliano Guardabassi <emilianojguardabassi@hotmail.com>
2024-11-01 14:11:13 -0300

    added cmpconfig, inlayhints, and colorschemes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  -- add dracula
+  { "Mofiqul/dracula.nvim" },
+  -- add monokai
+  { "nxstynate/monokai.nvim"},
+  --add 
+  { "ellisonleao/gruvbox.nvim" },
+
+
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
